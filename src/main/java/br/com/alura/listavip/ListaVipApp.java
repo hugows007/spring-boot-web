73cbce033aa18a0f6a2f4b24899066f6123706d2
Hugo Bezerra Pimentel <hugobezerrapimentel@gmail.com>
2020-03-17 19:30:45 -0300

    Primeiro commit.

---
+++ src/main/java/br/com/alura/listavip/ListaVipApp.java
@@ -0,0 +1,13 @@
+package br.com.alura.listavip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaVipApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaVipApp.class, args);
+	}
+	
+}
